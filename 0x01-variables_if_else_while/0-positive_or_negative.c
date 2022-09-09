b5d3c6a7f5beb8b66bb950a03a69fdceb323ddfe
lomsey41 <uc.salome2@gmail.com>
2022-09-09 16:44:21 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ * Return: Always 0 (Success)
+ */ */
+
+clear
