b978752d08b93da80b2b92360625ff8d1a35b26e
lomsey41 <uc.salome2@gmail.com>
2022-09-13 10:17:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
-#include "main.h"
+#include "main.h"
 /**
- * *main - Entry point, write the character c
- * *
- * *Return : on success 1
- *
- * */
-int  main(void)
+ * * main - prints Putchar
+ *  *
+ *  * Description: prints _putchar
+ *  *
+ *  * Return: 0 (Success)
+ *  */
+int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u ');
-	_putchar('t ');
-	_putchar('c ');
-	_putchar('h ');
-	_putchar('a ');
-	_putchar('r ');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
