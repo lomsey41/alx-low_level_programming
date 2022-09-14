e88123f88b64c78f3b6d2ee3bb3b26273ac1f7b8
lomsey41 <uc.salome2@gmail.com>
2022-09-13 19:59:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 /**
 * main - Entry point
 *
-* Description:  prints _putchar using putchar prototype
+* Description: prints _putchar using putchar prototype
 *
 * Return: Always 0 (Success)
 */
-nt main(void)
+int main(void)
 {
 char str[] = "_putchar";
-nt ch;
+int ch;
 for (ch = 0; ch < 8; ++ch)
 _putchar(str[ch]);
 _putchar('\n');
