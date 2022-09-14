e08f257761f81fc67359f5658de6c89a736bd1a6
lomsey41 <uc.salome2@gmail.com>
2022-09-14 07:52:30 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
 */
 void print_alphabet(void)
 {
-		int i;
+int i;
 
-		for (i = 'a'; i <= 'z'; i++)
-		{
-		_putchar(i);
-		}
-		_putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 
 }
