632ff49a47ee74a7ad5f599f7251db011746b6b1
lomsey41 <uc.salome2@gmail.com>
2022-09-19 10:05:07 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <studio.h>
+
+int _putchar(char c)
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +15,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif /* MAIN_H */
