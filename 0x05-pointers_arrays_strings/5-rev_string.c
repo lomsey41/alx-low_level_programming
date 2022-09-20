3c8722ae6b2b8abe4f236b821525d856a9311488
lomsey41 <uc.salome2@gmail.com>
2022-09-20 09:49:51 -0700

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,13 @@ void rev_string(char *s);
 	int len = 0, i = 0;
 	char aux;
 
-	while (s[len] != '\n')
+	while (s[len] != '\0')
 		len++;
 
 	while (i < len--)
 	{
 		aux = s[i];
 		s[i++] = s[len];
-		s[len] = aux
+		s[len] = aux;
 	}
-
 }
