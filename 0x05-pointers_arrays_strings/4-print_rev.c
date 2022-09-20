a8d59b704ee0e05c66ec8a92c1fce38857a1f8c8
lomsey41 <uc.salome2@gmail.com>
2022-09-20 13:48:32 -0700

    string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,16 @@
  * Return: Always (success)
  */
 void print_rev(char *s)
+
 {
 	int length = 0;
 
-	while ([length] != '\0')
+	while ([--length] != '\0')
 	{
 		length++;
 	}
 	while (length > 0)
 		_putchar(s[--length]);
+
 	_putchar(10);
 }
