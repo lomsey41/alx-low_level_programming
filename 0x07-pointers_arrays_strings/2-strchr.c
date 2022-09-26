07ec9ce396c588997bb8f10e6195b7466f882ef0
lomsey41 <uc.salome2@gmail.com>
2022-09-26 12:21:27 -0700

    string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,10 +6,10 @@
  * @s: an input string to search in
  * @c: an input character to locate into string s
  * Return: returns pointer to c position
+ * @s: Return: return NULL if the character isn't found.
  */
 char *_strchr(char *s, char c)
 {
-
 	while (*s)
 	{
 		if (c == *s)
