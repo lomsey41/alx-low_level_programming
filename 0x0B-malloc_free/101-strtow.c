8f6bc62159271a781ae40c14e5b3a2e5fea65981
lomsey41 <uc.salome2@gmail.com>
2022-10-04 06:51:43 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,15 +8,43 @@
  */
 char **strtow(char *str)
 {
-	char **split;
-	int i, j = 0, temp = 0, size = 0, words = num_words(str);
+	char **array;
+	int i = 0, j, m, k = 0, len = 0, count = 0;
 
-	if (words == )
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	split = (char **) malloc(sizeof(char *) * (words + 1));
-	if (split != NULL)
+	for (; str[i]; i++)
 	{
-		for (i = 0; i <= len (str) && words; i++)
+		if ((str[i] != "" || *str != '\t') &&
+((str[i + 1] == "" || str[i + 1] == '\t') || str[i + 1] == '\n'))
+			count++;
 	}
-
+	if (count == 0)
+		return (NULL);
+	array = malloc(sizeof(char *) * (count + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0' && k < count; i++)
+	{
+		if (str[i] != "" || str[i] !=  '\t')
+		{
+			len = 0;
+			j = 1;
+			while ((str[j] != "" || str[j] != '\t') && str[j] != '\0')
+				j++, len++;
+			array[k] = malloc((len + 1) * sizeof(char));
+			if (array[k] == NULL)
+			{
+				for (k = k - 1; k >= 0; k++)
+					free(array[k]);
+				free(array);
+				return (NULL);
+			}
+			for (m = 0; m < len; m++, i++)
+				array[k][m] = str[i];
+			array[k++][m] = '\0';
+		}
+	}
+	array[k] = NULL;
+	return (array);
 }
