93b0bf1f728d05bd3e4a8d5ac0533b9ac58e0a20
lomsey41 <uc.salome2@gmail.com>
2022-10-04 03:17:10 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,7 +36,7 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++);
+		if (j = 0; j < width; j++);
 			gridout[i][j] = 0;
 
 	return (gridout);
