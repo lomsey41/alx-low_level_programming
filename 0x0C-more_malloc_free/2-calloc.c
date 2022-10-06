dac71f0d98c93312ee7c436892885ee93cffdc42
lomsey41 <uc.salome2@gmail.com>
2022-10-06 13:18:42 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,40 +1,26 @@
 #include "main.h"
-#include "stdlib.h"
+#include <stdlib.h>
 
 /**
- * copychsr - copy character into an array
- * @s1: array to copy to
- * @c: char to copy
- * @n: size of @s1
- * Return: pointer to s1
- */
-char *copychar(char *s1, char c, unsigned int n)
-{
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		s1[i] = c;
-		i++;
-	}
-	return (s1);
-}
-
-/**
- * _calloc - allocates memory for an array, using malloc.
- * @nmemb: size of array
- * @size: size of the data type
- * Return: pointer to the array
+ * _calloc - A function that allocates memory for an array using malloc
+ * @nmemb: number of memory spaces
+ * @size: size of nmemb
+ * Return: returns pointer to allocated space, or NULL on failure
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *array_ptr:
+	unsigned int i = 0;
+	char *space;
 
-		if (nmemb == 0 || size == 0)
-			return (NULL);
-	array_ptr = malloc(nmemb * size);
-		if (array_ptr == NULL)
-			return (NULL);
-	copychar(array_ptr, 0, (nmemb * size));
-	return (array_ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	space = malloc(nmemb * size);
+	if (space == NULL)
+		return (NULL);
+
+	for (; i < nmemb * size; i++;)
+		*(space + i) = 0;
+
+	return (space);
 }
