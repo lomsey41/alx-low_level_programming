4718edcd8117b9e3b48e1bf98b52724b993479ec
lomsey41 <uc.salome2@gmail.com>
2022-10-06 16:08:21 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,27 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- * _calloc - A function that allocates memory for an array using malloc
- * @nmemb: number of memory spaces
- * @size: size of nmemb
- * Return: returns pointer to allocated space, or NULL on failure
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i = 0;
-	char *space;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	space = malloc(nmemb * size);
-	if (space == NULL)
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
 		return (NULL);
 
-	for (; i < nmemb * size, i++);
-		*(space + i) = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
-	return (space);
+	return (p);
 }
