6cf8c99a17fbf55260ffea5f4133d2689493859a
lomsey41 <uc.salome2@gmail.com>
2022-10-06 12:42:53 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
+char *copychar(char *s1, char c, unsigned int n);
 
 #endif
